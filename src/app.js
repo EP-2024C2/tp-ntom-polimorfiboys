bedb164e06e71d2905b5a72c39e050230941d314
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2024-10-08 16:51:33 -0300

    modelos y relaciones creadas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log(`Trabajo Practico de Estrategias de Persistencia.....`)
+const express = require('express')
+const app = express()
+const PORT = 3001
+const db = require('./models')
+app.use(express.json())
+
+app.listen(PORT, async ()=>{
+    console.log(`Aplicacion iniciada en el puerto ${PORT}`)
+    db.sequelize.sync({force:true})
+})
